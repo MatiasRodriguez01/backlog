450d71948dc14e6f16aead143d3c9e2787b03a53
santi bazan <santiago.n.bazan@outlook.com>
2025-03-26 09:34:49 -0300

    Correccion lista tarea

--- src/components/ul/ListaTareas/ListaTareas.tsx
+++ src/components/ul/ListaTareas/ListaTareas.tsx
@@ -1,44 +1,44 @@
-
-import Tarea from '../Tarea/Tarea'
-import styles from './ListaTareas.module.css'
+import Tarea from "../Tarea/Tarea";
+import styles from "./ListaTareas.module.css";
 
 const ListaTareas = () => {
-    return (
-        <>
-            <div className={styles.containerPrincipal}>
-                <div className={styles.titulo}>
-                    <h2>Nombre de la sprint</h2>
-                </div>
-                <div className={styles.buttonAddTarea}>
-                    <h2>Tareas en la sprint: </h2>
-                    <button>
-                        <p>Crear Tarea</p>
-                        <span className="material-symbols-outlined">playlist_add</span>
-                    </button>
-                </div>
-                <div className={styles.containerTareas}>
-                    <div className={styles.pendiente}>
-                        <div className={styles.titulo}>
-                            <h2>Pendiente</h2>
-                        </div>
-                        <Tarea/>
-                    </div>
-                    <div className={styles.proceso}>
-                        <div className={styles.titulo}>
-                            <h2>En proceso</h2>
-                        </div>
-                        <Tarea />
-                    </div>
-                    <div className={styles.completado}>
-                        <div className={styles.titulo}>
-                            <h2>completado</h2>
-                        </div>
-                        <Tarea/>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className={styles.containerPrincipal}>
+        <div className={styles.titulo}>
+          <h2>Nombre de la sprint</h2>
+        </div>
+        <div className={styles.buttonAddTarea}>
+          <h2>Tareas en la sprint: </h2>
+          <button>
+            <p>Crear Tarea</p>
+            <span className="material-symbols-outlined">playlist_add</span>
+          </button>
+        </div>
+        <div className={styles.containerTareas}>
+          <div className={styles.pendiente}>
+            <div className={styles.titulo}>
+              <h2>Pendiente</h2>
             </div>
-        </>
-    )
-}
+            <Tarea />
+            <Tarea />
+          </div>
+          <div className={styles.proceso}>
+            <div className={styles.titulo}>
+              <h2>En proceso</h2>
+            </div>
+            <Tarea />
+          </div>
+          <div className={styles.completado}>
+            <div className={styles.titulo}>
+              <h2>completado</h2>
+            </div>
+            <Tarea />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ListaTareas
+export default ListaTareas;
