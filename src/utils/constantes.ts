55cc33adec8af3b5d5ddf90947e474eed952fd8e
matias rodriguez <matiasrodriguez3002@gmail.com>
2025-03-30 14:51:06 -0300

    se modifico el src/utils/constantes.ts

--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -1,3 +1,4 @@
 export const API_PROYECTS = "http://localhost:3000/proyectoList"
+export const API = "http://localhost:3000/proyectoList/proyectos/tareas"
 
 export const API_BACKLOG = "http://localhost:3000/backlog"
